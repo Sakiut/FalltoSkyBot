87ed60db3eda7cb18bd952cf42dd87ad5eaa9989
Sakiut <sakiut@alwaysdata.net>
2017-05-11 07:56:58 +0200

    Fix `.perms`

--- libraries/library.py
+++ libraries/library.py
@@ -20,90 +20,90 @@ fileName = './config.txt'
 #####################################################################################################################################################
 
 def get_user_roles(user):
-    """Retourne une série de str, les roles de l'utilisateur"""
+	"""Retourne une série de str, les roles de l'utilisateur"""
 
-    roles = user.roles
-    RoleList = []
-    RoleStr = ""
+	roles = user.roles
+	RoleList = []
+	RoleStr = ""
 
-    for x in range(len(roles)):
-        role = roles[x].name
-        RoleList.append(role)
+	for x in range(len(roles)):
+		role = roles[x].name
+		RoleList.append(role)
 
-    del RoleList[0]
+	del RoleList[0]
 
-    for x in range(len(RoleList)):
-        role = RoleList[x]
-        RoleStr += role + ", "
+	for x in range(len(RoleList)):
+		role = RoleList[x]
+		RoleStr += role + ", "
 
-    RoleFinal = RoleStr.rstrip(', ')
+	RoleFinal = RoleStr.rstrip(', ')
 
-    return RoleFinal
+	return RoleFinal
 
 #####################################################################################################################################################
 
 def getServerRules():
 
-    fileName = './rules.txt'
-    with open(fileName) as f: rulesLines = f.read()
+	fileName = './rules.txt'
+	with open(fileName) as f: rulesLines = f.read()
 
-    return rulesLines
+	return rulesLines
 
 #####################################################################################################################################################
 
 def getToken():
 
-    with open(fileName) as f: lines = f.read().splitlines()
+	with open(fileName) as f: lines = f.read().splitlines()
 
-    for line in lines:
-        if line.startswith('token='):
-            getLine = line
-            Line = getLine.split('=')
-            token = Line[1]
+	for line in lines:
+		if line.startswith('token='):
+			getLine = line
+			Line = getLine.split('=')
+			token = Line[1]
 
-    return token
+	return token
 
 #####################################################################################################################################################
 
 def getApiKey():
 
-    with open(fileName) as f: lines = f.read().splitlines()
+	with open(fileName) as f: lines = f.read().splitlines()
 
-    for line in lines:
-        if line.startswith('apikey='):
-            getLine = line
-            Line = getLine.split('=')
-            ApiKey = Line[1]
+	for line in lines:
+		if line.startswith('apikey='):
+			getLine = line
+			Line = getLine.split('=')
+			ApiKey = Line[1]
 
-    return ApiKey
+	return ApiKey
 
 #####################################################################################################################################################
 
 def getServerIP():
 
-    with open(fileName) as f: lines = f.read().splitlines()
+	with open(fileName) as f: lines = f.read().splitlines()
 
-    for line in lines:
-        if line.startswith('serverip='):
-            getLine = line
-            Line = getLine.split('=')
-            ServerIP = Line[1]
+	for line in lines:
+		if line.startswith('serverip='):
+			getLine = line
+			Line = getLine.split('=')
+			ServerIP = Line[1]
 
-    return ServerIP
+	return ServerIP
 
 #####################################################################################################################################################
 
 def getWebSite():
 
-    with open(fileName) as f: lines = f.read().splitlines()
+	with open(fileName) as f: lines = f.read().splitlines()
 
-    for line in lines:
-        if line.startswith('website='):
-            getLine = line
-            Line = getLine.split('=')
-            WebSite = Line[1]
+	for line in lines:
+		if line.startswith('website='):
+			getLine = line
+			Line = getLine.split('=')
+			WebSite = Line[1]
 
-    return WebSite
+	return WebSite
 
 #####################################################################################################################################################
 
@@ -158,52 +158,52 @@ def getServerEmojis(emojisObjects:list):
 
 def getLDStats(pseudo):
 
-    pseudo = str(pseudo)
-    url = "http://leveldown.fr/profile/{}".format(pseudo)
-    fileName = "./ldsource/{}.html".format(pseudo)
-    request.urlretrieve(url, fileName)
+	pseudo = str(pseudo)
+	url = "http://leveldown.fr/profile/{}".format(pseudo)
+	fileName = "./ldsource/{}.html".format(pseudo)
+	request.urlretrieve(url, fileName)
 
-    with open(fileName) as f: lines = f.read().splitlines()
+	with open(fileName) as f: lines = f.read().splitlines()
 
-    Names = []
-    Values = []
+	Names = []
+	Values = []
 
-    for line in lines:
-    	if line.startswith('				<div><span>'):
-    		getLine = line.rstrip("</div>")
-    		Line = getLine.replace("<div><span", "")
-    		Line = Line.replace("</span>", "|")
-    		Line = Line.split(">")
-    		Line = Line[1]
-    		Line = Line.split("|")
+	for line in lines:
+		if line.startswith('				<div><span>'):
+			getLine = line.rstrip("</div>")
+			Line = getLine.replace("<div><span", "")
+			Line = Line.replace("</span>", "|")
+			Line = Line.split(">")
+			Line = Line[1]
+			Line = Line.split("|")
 
-    		Names.append(Line[0])
-    		Values.append(Line[1])
+			Names.append(Line[0])
+			Values.append(Line[1])
 
-    LDStats = {'Names' : Names, 'Values' : Values}
+	LDStats = {'Names' : Names, 'Values' : Values}
 
-    return LDStats
+	return LDStats
 
 #####################################################################################################################################################
 
 def getLDIcon(pseudo):
 
-    pseudo = str(pseudo)
-    url = "http://leveldown.fr/profile/{}".format(pseudo)
-    fileName = "./ldsource/{}.html".format(pseudo)
-    request.urlretrieve(url, fileName)
+	pseudo = str(pseudo)
+	url = "http://leveldown.fr/profile/{}".format(pseudo)
+	fileName = "./ldsource/{}.html".format(pseudo)
+	request.urlretrieve(url, fileName)
 
-    with open(fileName) as f: lines = f.read().splitlines()
+	with open(fileName) as f: lines = f.read().splitlines()
 
-    Names = []
-    Values = []
+	Names = []
+	Values = []
 
-    for line in lines:
-    	if line.startswith('							<div class="avatar"'):
-    		getLine = line.rstrip(');"></div>')
-    		Line = getLine.split("(")
-    		Line = Line[1]
+	for line in lines:
+		if line.startswith('							<div class="avatar"'):
+			getLine = line.rstrip(');"></div>')
+			Line = getLine.split("(")
+			Line = Line[1]
 
-    return Line
+	return Line
 
 #####################################################################################################################################################
