3ad3ec3d0215098693950de385079126f8fbc540
Sakiut <sakiut@alwaysdata.net>
2017-05-18 23:18:41 +0200

    Added `anilist.py`

---
+++ libraries/anilist.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from random import shuffle
+import time
+from decimal import *
+import sys
+import datetime
+
+from libraries.perms import *
+from libraries.library import *
+
+from urllib import request
+import linecache
+import ast
+
+#####################################################################################################################################################
+
+def auth(params):
+	q = requests.post('https://anilist.co/api/auth/access_token', params=params)
+	auth = q.json()
+	token = auth['access_token']
+
+	return token
+
+#####################################################################################################################################################
+
+def getAnimeInfo(anime, token):
+	anime = str(anime)
+	q = requests.get('https://anilist.co/api/anime/search/{0}'.format(anime), params={'access_token':token})
+	data = q.json()
+	data = filter(lambda x: x.get("title_english") == anime, data)
+	data = list(data)
+	data = data[0]
+
+	return data
+
+#####################################################################################################################################################
+
+def getAnimeGenres(data):
+	List = data['genres']
+	genres = ""
+
+	for genre in List:
+		genres += genre + ", "
+
+	genres = genres.rstrip(', ')
+
+	return genres
+
+#####################################################################################################################################################
+
+def formatAnimeDescription(data):
+	desc = data['description']
+	desc = desc.replace("<br>", "")
+	return desc
+
+#####################################################################################################################################################
+
+def formatAnimeDate(data):
+
+	Message = ""
+	date = data['start_date']
+
+	date = str(date)
+	split1 = date.split("T")
+	date = split1[0]
+	split2 = date.split("-")
+	date = "{0}/{1}/{2}".format(split2[2], split2[1], split2[0])
+
+	Message += "Started : " + date
+	date = data['end_date']
+
+	if date == None :
+		return Message
+	else:
+		date = str(date)
+		split1 = date.split("T")
+		date = split1[0]
+		split2 = date.split("-")
+		date = "{0}/{1}/{2}".format(split2[2], split2[1], split2[0])
+
+		Message += " | Finished : " + date
+
+		return Message
+
+#####################################################################################################################################################
+
+def getMangaInfo(manga, token):
+	manga = str(manga)
+	q = requests.get('https://anilist.co/api/manga/search/{0}'.format(manga), params={'access_token':token})
+	data = q.json()
+	data = filter(lambda x: x.get('type') == 'Manga', data)
+	data = list(data)
+	data = data[0]
+
+	return data
+
+#####################################################################################################################################################
