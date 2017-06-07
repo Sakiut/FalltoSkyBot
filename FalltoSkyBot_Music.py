def11c327e0b335eb16bfa83797146f959ccec30
Sakiut <sakiut@alwaysdata.net>
2017-06-07 23:06:58 +0200

    Minor updates

--- FalltoSkyBot_Music.py
+++ FalltoSkyBot_Music.py
@@ -1595,6 +1595,7 @@ class RSS:
     @commands.command(pass_context=True, no_pm=True)    
     async def forceupdate(self, ctx):
         """[ADMIN] Teste le système d'update de flux RSS"""
+        await self.bot.delete_message(ctx.message)
         if ctx.message.author.server_permissions.administrator == True:
             update = youtube.update(self.feed)
             if update != "304":
@@ -1616,6 +1617,7 @@ class RSS:
     @commands.command(pass_context=True, no_pm=True)
     async def updatelastentry(self, ctx):
         """[ADMIN] Force la dernière entrée RSS"""
+        await self.bot.delete_message(ctx.message)
         if ctx.message.author.server_permissions.administrator == True:
             entry = youtube.getLastEntry()
             YTEmbed = discord.Embed()
